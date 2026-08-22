bd819ed15e26bdc27fc9556af1ba96291af15047
Venkat <venkatedupuganti0311@gmail.com>
2026-08-22 19:09:55 +0530

    1827A

--- R-1100/1827A.cpp
+++ R-1100/1827A.cpp
@@ -1,8 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
+#define MOD (ll)(1e9+7)
 
+void solve() {
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    vector<int>b(n);
+    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<n;i++)cin>>b[i];
+
+    sort(a.begin(),a.end());
+    sort(b.rbegin(),b.rend());
+
+    long long ans=1;
+    for(int i=0;i<n;i++){
+        auto it=upper_bound(a.begin(),a.end(),b[i]);
+        int count=a.end()-it;
+
+        ans=ans*max(count-i,0LL);
+    }
+    ans=ans%MOD;
+    cout<<ans<<'\n';
 }
 
 int main() {
