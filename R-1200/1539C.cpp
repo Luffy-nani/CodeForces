bbb7e541b8649a1be14321d3e569c38bd0e7fc7a
Venkat <venkatedupuganti0311@gmail.com>
2026-09-03 15:49:00 +0530

    done

---
+++ R-1200/1539C.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long n,k,x;
+    cin>>n>>k>>x;
+    vector<long long>a(n);
+    for(int i=0;i<n;i++)cin>>a[i];
+
+    sort(a.begin(),a.end());
+
+    vector<long long>gaps;
+
+    for(int i=0;i<n-1;i++){
+        if(a[i+1]-a[i]> x)gaps.push_back(a[i+1]-a[i]);
+    }
+
+    long long ans=gaps.size()+1;
+    sort(gaps.begin(),gaps.end());
+
+    for(auto diff:gaps){
+       long long numberOfStudents = (diff - 1) / x;
+        if(k>=numberOfStudents){
+            ans--;
+            k-=numberOfStudents;
+        }
+    }
+
+    cout<<ans<<'\n';
+
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+}
